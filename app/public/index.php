7e423ba4b7c20a8b0a8f851cc5a83ccfbbb995df
Nicole <111709554+nicole440@users.noreply.github.com>
2023-05-12 18:59:01 -0400

    Add link to stylesheet

--- app/public/index.php
+++ app/public/index.php
@@ -4,10 +4,10 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<link rel="stylesheet" type="text/css" href="styles.css">
     <title>HUD-1 Settlement Statement</title>
 </head>
 <body>
-<?php echo "Hello, World!" ?>
 <table class="buyer-side">
 	<thead>  <!-- optional tag -->
 		<tr>
