94335b94ba69934c8c3a435fc380e0657f14f167
Nicole <111709554+nicole440@users.noreply.github.com>
2023-05-13 16:44:27 -0400

    Add controller

---
+++ app/public/controller.php
@@ -0,0 +1,15 @@
+<?php
+
+if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+  // process form data submitted via POST
+
+    $contract_sale_price = $_POST["contract_sale_price"]; 
+    $personal_property = $_POST["personal_property"];
+
+    $connection = new PDO('mariadb:host=localhost;dbname=hud;charset=utf8', 'root', 'root');
+    
+  }
+
+if ($_SERVER['REQUEST_METHOD'] == 'GET' && isset($_POST['method']) && $_POST['method'] == 'get') {
+  // process GET request
+}
